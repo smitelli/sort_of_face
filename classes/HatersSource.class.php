a660bff8912006299cefec2b64a8487a5b9527b3
Scott Smitelli <scott@smitelli.com>
2015-03-30 22:43:31 -0400

    committers gonna committ

--- classes/HatersSource.class.php
+++ classes/HatersSource.class.php
@@ -7,23 +7,33 @@
    */
 
   class HatersSource {
+    private $dictionary_file;
+
     /**
-     * Constructor function. Parses a config array for TODO
+     * Constructor function. Parses a config array for a 'dictionary_file' key.
      * @access public
      * @param array $config The configuration array
      */
     public function __construct($config) {
-
+      $this->dictionary_file = $config['dictionary_file'];
     }
 
     /**
      * Picks random words out of the system dictionary file and builds an "xers
      * gonna x" sentence around them.
+     * TODO: This is probably inefficient as hell.
      * @access public
      * @return string A piece of gibberish
      */
     public function getLine() {
-      return "Oh, hello.";
+      $dictionary = file_get_contents($this->dictionary_file);
+
+      $matches = array();
+      preg_match_all('/(^.+)er$/m', $dictionary, $matches);
+      shuffle($matches[1]);
+
+      $base_word = array_pop($matches[1]);
+      return "{$base_word}ers gonna {$base_word}";
     }
   }
 
